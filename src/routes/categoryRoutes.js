e50268a2b3d405e61634d1a33519b7699828bdcc
Alfarel <146792698+alfarelshadat@users.noreply.github.com>
2026-05-07 17:05:10 +0700

    up put2

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 router.get('/', CategoryController.getCategories);
 router.post('/', CategoryController.addCategory);
 router.put('/:id', CategoryController.updateCategory);
+router.delete('/:id', CategoryController.deleteCategory);
 export default router;
