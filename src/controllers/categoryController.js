a3e7217b13c40add3193f9b20bfad75ae9891b43
Alfarel <146792698+alfarelshadat@users.noreply.github.com>
2026-05-07 17:05:53 +0700

    put

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -42,6 +42,27 @@ export const CategoryController = {
 
   }
 
- }
+ },
+
+ async deleteCategory(req, res) {
+
+  try {
+
+    const result =
+      await CategoryModel.delete(
+        req.params.id
+      );
+
+    res.json(result);
+
+  } catch (err) {
+
+    res.status(400).json({
+      error: err.message
+    });
+
+  }
+
+  }
 };
 
