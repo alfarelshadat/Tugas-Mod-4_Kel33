dd70f61f807aecb78943b2ef10ef2b4e59c26fcd
Alfarel <146792698+alfarelshadat@users.noreply.github.com>
2026-05-07 17:47:41 +0700

    up

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get('/', LoanController.getLoans);
 router.post('/', LoanController.createLoan);
 router.post('/:id/return', LoanController.returnBook);
+router.put('/:id', LoanController.updateLoan);
+router.delete('/:id', LoanController.deleteLoan);
 export default router;
